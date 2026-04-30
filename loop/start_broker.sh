e3ecd0d1ef3c3cf310401e0279df86fda0394bdf
Sridhar Ayengar <sridhar@jagannath.ikickass.org>
2026-04-30 15:32:33 -0400

    scripts: remove Redis from start_broker.sh, fix stale queue topology output in start_loop.sh

--- loop/start_broker.sh
+++ loop/start_broker.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-# Starts ActiveMQ Artemis and Redis.
+# Starts ActiveMQ Artemis.
 #
 # First-time setup (run once):
 #   artemis create "$AI_IMAGE_ROOT/loop/artemis-broker"
-#   Then add the AMQP 0-9-1 acceptor to artemis-broker/etc/broker.xml:
-#     <acceptor name="amqp091">tcp://0.0.0.0:5672?protocols=AMQP;amqpCredits=1000</acceptor>
-#   See loop/artemis-amqp091-acceptor.xml for the snippet to add.
-#
-# Install: brew install activemq
+#   Then enable both acceptors in artemis-broker/etc/broker.xml:
+#     <acceptor name="amqp">tcp://0.0.0.0:5672?protocols=AMQP</acceptor>
+#     <acceptor name="stomp">tcp://0.0.0.0:61613?protocols=STOMP</acceptor>
+#   See INSTALL.md section 3 for details.
 AI_IMAGE_ROOT="${1:-${AI_IMAGE_ROOT:-}}"
 if [ -z "$AI_IMAGE_ROOT" ] && [ -f "$PWD/config.yaml" ] && [ -d "$PWD/loop" ]; then
     AI_IMAGE_ROOT="$PWD"
@@ -19,7 +18,6 @@ ARTEMIS_DATA=$(yq '.broker.artemis_data' "$CFG")
 [ "$ARTEMIS_DATA" = "auto" ] && ARTEMIS_DATA="$AI_IMAGE_ROOT/loop/artemis-broker"
 
 "$ARTEMIS_DATA/bin/artemis" run &
-redis-server --daemonize yes
 
 _HOST=$(hostname 2>/dev/null || echo "localhost")
 echo "==> Artemis console: http://${_HOST}:8161  (admin / admin)"
